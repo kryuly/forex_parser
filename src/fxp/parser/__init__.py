d3b16d43937bca341587d9cafd06ce3d6aef1dc9
Yulyana Kruhlikava <yuli_terra@mail.ru>
2021-01-07 18:29:18 +0300

    parser for preview

---
+++ src/fxp/parser/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/87.0.4280.88 Safari/537.36"
+)
+
+HOST = "https://www.investing.com"
+PREVIEW_URL = "{}/news/forex-news/{}"
